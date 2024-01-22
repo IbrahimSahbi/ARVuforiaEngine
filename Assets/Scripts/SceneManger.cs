6280f663aa0cc32afbb879e4b670810806d53905
IbrahimSahbi <ibrahim.sahbi@3lines.com.sa>
2024-01-22 13:03:24 +0300

    andriod app buid done

--- Assets/Scripts/SceneManger.cs
+++ Assets/Scripts/SceneManger.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class SceneManger : MonoBehaviour
 {
     public int SceneNumber;
-    public void OnButtonCLick()
+    public void OnButtonCLick(int SceneNumber)
     {
         SceneManager.LoadScene(SceneNumber);
     }
